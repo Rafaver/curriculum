dce7c1a62109b0ffd6106cf8523540be77296403
Rafaver <150279363+Rafaver@users.noreply.github.com>
2023-11-09 14:56:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,37 @@
-from flask import Flask, render_template  
+from flask import Flask, render_template, request
 import requests
-from dotenv import load_dotenv,dotenv_values
+from datetime import datetime
+from dotenv import load_dotenv, dotenv_values
+import os
 
-config= dotenv_values('.env')
+config = dotenv_values('.env')
 
 app = Flask(__name__)
+
+# Ajusta la ubicación de las plantillas
+app.template_folder = 'Templates'
+
 def get_weather_data(city):
-    API_KEY = config ['API_KEY']
-    url = f'https://api.openweathermap.org/data/2.5/weather?appid={API_KEY}&q={city}&lang=es&units=metric'
+    API_KEY = config['API_KEY']
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&lang=es&units=metric'
     r = requests.get(url).json()
-    print(r)
     return r
 
+@app.route('/weather', methods=['GET', 'POST'])
+def weather():
+    weather_data = None
+    if request.method == 'POST':
+        city = request.form['city']
+        weather_data = get_weather_data(city)
+        if weather_data['cod'] == 200:
+            weather_data = {
+                'city_name': city,
+                'temperature': weather_data['main']['temp'],
+                'humidity': weather_data['main']['humidity'],
+                'description': weather_data['weather'][0]['description'],
+                'date_time': datetime.fromtimestamp(weather_data['dt']).strftime('%Y-%m-%d %H:%M:%S')
+            }
+    return render_template('weather.html', weather_data=weather_data)
 
 @app.route('/al')
 def hello_al():
@@ -24,8 +44,7 @@ def hello_albert():
 
 @app.route('/alfred')
 def hello_alfred():
-    return render_template("about.html")  
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
-
