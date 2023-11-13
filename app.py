6cee68a6dbc69946aa80cb2727d2fac6591afb45
Rafaver <150279363+Rafaver@users.noreply.github.com>
2023-11-13 00:30:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 import requests
 from datetime import datetime
 from dotenv import load_dotenv, dotenv_values
@@ -7,44 +7,52 @@ import os
 config = dotenv_values('.env')
 
 app = Flask(__name__)
-
-# Ajusta la ubicación de las plantillas
 app.template_folder = 'Templates'
+
+saved_cities = []  # Lista para almacenar temporalmente las ciudades guardadas
 
 def get_weather_data(city):
     API_KEY = config['API_KEY']
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&lang=es&units=metric'
     r = requests.get(url).json()
-    return r
+    if r.get('cod') == 200:
+        return {
+            'city_name': city,
+            'temperature': r['main']['temp'],
+            'humidity': r['main']['humidity'],
+            'description': r['weather'][0]['description'],
+            'date_time': datetime.fromtimestamp(r['dt']).strftime('%Y-%m-%d %H:%M:%S')
+        }
+    return None
 
-@app.route('/weather', methods=['GET', 'POST'])
+def save_city(city_data):
+    saved_cities.append(city_data)
+    return {'success': True}
+
+def delete_city(city):
+    saved_cities[:] = [city_data for city_data in saved_cities if city_data['city_name'] != city]
+
+@app.route('/')
+def index():
+    return render_template('weather.html', saved_cities=saved_cities)
+
+@app.route('/weather', methods=['POST'])
 def weather():
-    weather_data = None
-    if request.method == 'POST':
-        city = request.form['city']
-        weather_data = get_weather_data(city)
-        if weather_data['cod'] == 200:
-            weather_data = {
-                'city_name': city,
-                'temperature': weather_data['main']['temp'],
-                'humidity': weather_data['main']['humidity'],
-                'description': weather_data['weather'][0]['description'],
-                'date_time': datetime.fromtimestamp(weather_data['dt']).strftime('%Y-%m-%d %H:%M:%S')
-            }
-    return render_template('weather.html', weather_data=weather_data)
+    city = request.args.get('city') or request.form.get('city')
+    weather_data = get_weather_data(city)
+    return jsonify(weather_data)
 
-@app.route('/al')
-def hello_al():
-    get_weather_data('Guayaquil')
-    return get_weather_data('Guayaquil')  
+@app.route('/save_city', methods=['POST'])
+def save_city_route():
+    city_data = request.get_json()
+    response = save_city(city_data)
+    return jsonify(response)
 
-@app.route('/albert')
-def hello_albert():
-    return render_template("ug.html")  
-
-@app.route('/alfred')
-def hello_alfred():
-    return render_template("about.html")
+@app.route('/delete', methods=['POST'])
+def delete():
+    city = request.form['city']
+    delete_city(city)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
